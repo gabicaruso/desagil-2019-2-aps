fd2d3c996e0be33fbc4253ca2f9d9d89180d748d
Gabriela Caruso <gabriela.caruso@yahoo.com.br>
2019-09-17 18:03:27 -0300

    aps2

--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/FixedPanel.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/FixedPanel.java
@@ -26,7 +26,7 @@ public class FixedPanel extends JPanel {
     // Fixar posição e tamanho também não é uma boa prática,
     // pois o normal é deixar o layout decidir. Novamente,
     // escolhemos fazer isso aqui para priorizar simplicidade.
-    protected Component add(Component comp, int x, int y, int width, int height) {
+    protected void add(Component comp, int x, int y, int width, int height) {
 
         // Usa a implementação original para adicionar.
         super.add(comp);
@@ -34,6 +34,5 @@ public class FixedPanel extends JPanel {
         // Redefine posição e tamanho da componente.
         comp.setBounds(x, y, width, height);
 
-        return comp;
     }
 }
--- src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/view/GateView.java
@@ -8,6 +8,10 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 public class GateView extends FixedPanel implements ItemListener {
+    private static final int BORDER = 10;
+    private static final int SWITCH_SIZE = 18;
+    private static final int GATE_WIDTH = 90;
+    private static final int GATE_HEIGHT = 120;
 
     private final Switch[] switches;
     private final Gate gate;
@@ -15,7 +19,7 @@ public class GateView extends FixedPanel implements ItemListener {
     private final JCheckBox outputBox;
 
     public GateView(Gate gate) {
-        super(100,100);
+        super(BORDER + SWITCH_SIZE + GATE_WIDTH + SWITCH_SIZE + BORDER, GATE_HEIGHT);
         this.gate = gate;
 
         int inputSize = gate.getInputSize();
@@ -35,9 +39,13 @@ public class GateView extends FixedPanel implements ItemListener {
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+        int y = -(SWITCH_SIZE / 2);
+        int step = (GATE_HEIGHT / (inputSize + 1));
+
         add(inputLabel);
         for (JCheckBox inputBox : inputBoxes) {
-            add(inputBox);
+            y += step;
+            add(inputBox, BORDER, y, SWITCH_SIZE, SWITCH_SIZE);
             inputBox.addItemListener(this);
         }
 
@@ -68,5 +76,6 @@ public class GateView extends FixedPanel implements ItemListener {
     public void itemStateChanged(ItemEvent event) {
         update();
     }
+
 }
 
